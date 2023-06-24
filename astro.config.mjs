5ad8d2490f7ad15ec22b8b3a06fcb150760e863c
Kitorosano <rosanoesteban2@gmail.com>
2023-06-23 21:15:39 -0300

    setting up github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  site: 'https://kitorosano.github.io',
+  base: '/fix-my-english'
 });
